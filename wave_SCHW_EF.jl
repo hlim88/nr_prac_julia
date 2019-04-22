964860cd34b792ea68db3db5a728a6582f38eb4f
Hyun Lim <hylim1988@gmail.com>
2019-04-22 17:43:59 -0600

    Update B matrix

--- wave_SCHW_EF.jl
+++ wave_SCHW_EF.jl
@@ -33,8 +33,6 @@ s[1, :] = idsignum * amp * np.exp(-(x_grid.-x_c).^2/width^2)
 # Define matrix A for our discretization scheme
 
 A = np.zeros((2*N, 2*N))
-B = np.zeros((2*N, 2*N))
-
 for i = 1:N
     if i == 1
         for j = 1:2*N
@@ -114,6 +112,121 @@ for i = N+1:2*N
         end
     end
 end
-    show(A)
-    show(A[1,1])
 
+# Define matrix B
+B = np.zeros((2*N, 2*N))
+
+for i = 1:N
+    if i == 1
+        for j = 1:2*N
+            if j==1
+                B[i,j] = 1
+            end
+            if j==N+1
+                B[i,j] = -cfl/4
+            end
+            if j==N+2
+                B[i,j] = cfl/4
+            end
+        end
+    elseif i == N
+        for j = 1:2*N
+            if j==N
+                B[i,j] = 1
+            end
+            if j==2*N-1
+                B[i,j] = -cfl/4
+            end
+            if j==2*N
+                B[i,j] = cfl/4
+            end
+        end
+    else 
+        for j = 1:2*N
+            if j==i
+                B[i,j] = 1
+            end
+            if j==N+i
+                B[i,j] = -cfl/4
+            end
+            if j==N+i+2
+                B[i,j] = cfl/4
+            end
+        end
+    end
+end
+    
+for i = N+1:2*N
+    if i == N+1
+        for j = 1:2*N
+            if j==N+1
+                B[i,j] = 1
+            end
+            if j==1
+                B[i,j] = -cfl/4
+            end
+            if j==2
+                B[i,j] = cfl/4
+            end
+        end
+    elseif i == 2*N
+        for j = 1:2*N
+            if j==2*N
+                B[i,j] = 1
+            end
+            if j==N-1
+                B[i,j] = -cfl/4
+            end
+            if j==N
+                B[i,j] = cfl/4
+            end
+        end
+    else 
+        for j = 1:2*N
+            if j==i
+                B[i,j] = 1
+            end
+            if j==i-N
+                B[i,j] = -cfl/4
+            end
+            if j==i+2-N
+                B[i,j] = cfl/4
+            end
+        end
+    end
+end
+
+
+# Update routine
+function update(t_steps)
+    u = np.zeros(2*N)
+      for i = 1:2*N
+          if i<N+1
+              u[i] = r[t_steps,i]
+          else
+              u[i] = s[t_steps,i-N]
+          end
+      end
+end
+
+# Update r and s using the solved-for vector u at t_step
+function update_rs(u_sol, t_steps)
+    for i = 1:2*N
+        if i<N+1
+            r[t_steps, i] = u_sol[i]
+        else
+            s[t_steps, i-N+1] = u_sol[i]
+            # Applying Sommerfeld condition for boundray
+            s[t_steps, 1] = r[t_steps,1]
+            s[t_steps, N] = -r[t_steps,N]
+        end
+    end
+end
+
+# Solve the equations
+for i = 1:t_steps
+    u = update(i)
+    rhs = B.*u
+    u_sol = np.linalg.solve(A,rhs)
+    update_rs(u_sol,n)
+end
